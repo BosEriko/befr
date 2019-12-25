33c5ce59276aa29f455bbe05e44f8924a8f87a82
BosEriko <bos.eriko@gmail.com>
2019-12-25 22:12:19 +0800

    add a few options

--- lib/befr.rb
+++ lib/befr.rb
@@ -1,5 +1,29 @@
+require "optparse"
+
 class Befr
-  def self.hi
-    puts "Hello world!"
+  def self.hello
+    puts "hello"
+    exit
+  end
+
+  def self.world 
+    puts "world"
+    exit
   end
 end
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: backpack [options]"
+
+  opts.on("-H", "--hello", "Print Hello") do
+    Befr::hello
+  end
+
+  opts.on("-W", "--world", "Print World") do
+    Befr::world
+  end
+end
+
+parser.parse!
+
+puts parser
